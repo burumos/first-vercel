cf8f2a7b5146f992afd356dfe4816c044e697210
burumos <naoki19940317@yahoo.co.jp>
2024-11-06 18:18:40 +0900

    cp.16

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,24 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  { params }: { params: Promise<{ id: string }>},
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById((await params).id),
+    fetchCustomers(),
+  ]);
+
+  const invoiceUser = customers?.find((customer) => customer.id === invoice.customer_id);
+
+  return {
+    title: 'Edit invoice: ' + invoiceUser?.name,
+  }
+}
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
